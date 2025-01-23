fa56cca95a383582e9ca8e42503be7c28280fed9
Vikas Pritam <pritam.developer.1@gmail.com>
2025-01-23 22:54:15 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -21,11 +21,11 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
