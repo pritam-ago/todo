b3a01478ceb615491ca96604799149c3c0dc0818
Vikas Pritam <pritam.developer.1@gmail.com>
2025-01-24 14:40:23 +0530

    nothin

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://todo-blockchain-server.vercel.app',  
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://todo-blockchain-server.vercel.app',  
   headers: {
     'Authorization': `Bearer ${localStorage.getItem('token')}`,
   },
